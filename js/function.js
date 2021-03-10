d977eea5219e528050de23769925dcc4f59ea401
lesson1 <abinin99@bk.ru>
2021-03-10 23:44:57 +0300

    add card.html

--- js/function.js
+++ js/function.js
@@ -40,3 +40,20 @@ window.onclick = function (event) {
         }
     }
 }
+
+
+
+
+var numCount = document.getElementById('num_count');
+var plusBtn = document.getElementById('button_plus');
+var minusBtn = document.getElementById('button_minus');
+plusBtn.onclick = function () {
+    var qty = parseInt(numCount.value);
+    qty = qty + 1;
+    numCount.value = qty;
+}
+minusBtn.onclick = function () {
+    var qty = parseInt(numCount.value);
+    qty = qty - 1;
+    numCount.value = qty;
+}
